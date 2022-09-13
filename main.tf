8b6aebb9880b73292efc64da3efa54945b871e27
branfos <brandon.foster@hashicorp.com>
2022-09-13 12:56:27 -0500

    instance name

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
     region  = "us-east-1"
 }
 
-resource "aws_instance"  {
+resource "aws_instance" "test" {
   count         = var.size
   ami           = var.ami_id
   instance_type = "t2.micro"
